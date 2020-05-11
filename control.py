3b49f68f507b0512d6b85a5461578c93606fecd0
superroma <roman.eremin@devexpress.com>
2020-05-11 15:51:46 +0300

    fix

--- control.py
+++ control.py
@@ -148,6 +148,7 @@ def main():
                 calc_values()
                 if trigger:
                     trigger = False
+                print(beer_lines)
                 device.publish_data()
                 data_timer = DATA_SENDING_INTERVAL
 
